1da1ad3169b92b1032755b2ad4e55f35c7f8af0e
JMcunst <xnslqjtmghaf@gmail.com>
2023-12-27 20:50:59 +0900

    init

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:wiki_flutter_examples/widget/20231213/overlayportal.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Widget Examples',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const MyOverlayPortal());
+  }
+}
