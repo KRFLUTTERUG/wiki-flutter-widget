3f051eec7834b8766d80b561dded5e058b3481ce
jmcunst <xnslqjtmghaf@gmail.com>
2023-12-28 13:34:46 +0900

    feat : fittedbox

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:wiki_flutter_examples/package/20231025/fl_chart.dart';
+import 'package:wiki_flutter_examples/widget/20210123/fittedbox.dart';
 import 'package:wiki_flutter_examples/widget/20210123/stepper.dart';
 import 'package:wiki_flutter_examples/widget/20230602/draggable.dart';
 import 'package:wiki_flutter_examples/widget/20231213/gap.dart';
@@ -21,6 +22,6 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: const MyStepper());
+        home: const MyFittedBox());
   }
 }
--- lib/widget/20210123/README.md
+++ lib/widget/20210123/README.md
@@ -1,4 +1,9 @@
-# Stepper Widget
+# 1. Stepper Widget
 
-## 1. stepper.dart
+## a. stepper.dart
 ![stepper](https://github.com/KRFLUTTERUG/wiki-flutter-widget/assets/17956765/5ec1023b-3386-41b4-b0c5-32cdfcac2e0d)
+
+# 2. Stepper Widget
+
+## a. fittedbox.dart
+![image](https://github.com/KRFLUTTERUG/wiki-flutter-widget/assets/17956765/d49a92c2-6e01-497f-8687-d97cfe663849)
