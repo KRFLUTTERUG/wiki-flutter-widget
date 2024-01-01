2e5d9e3c8ab74335592d337921618d03813840b9
JMcunst <xnslqjtmghaf@gmail.com>
2024-01-01 14:12:44 +0900

    update : example

--- lib/widget/20210123/willpopscope.dart
+++ lib/widget/20210123/willpopscope.dart
@@ -1,45 +1,120 @@
 import 'package:flutter/material.dart';
 
-class MyWillPopScope extends StatefulWidget {
+void main() => runApp(const MyWillPopScope());
+
+class MyWillPopScope extends StatelessWidget {
   const MyWillPopScope({super.key});
 
   @override
-  State<MyWillPopScope> createState() => _MyWillPopScopeState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/home',
+      routes: <String, WidgetBuilder>{
+        '/home': (BuildContext context) => const _HomePage(),
+        '/two': (BuildContext context) => const _PageTwo(),
+      },
+    );
+  }
 }
 
-class _MyWillPopScopeState extends State<MyWillPopScope> {
+class _HomePage extends StatefulWidget {
+  const _HomePage();
+
+  @override
+  State<_HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<_HomePage> {
   @override
   Widget build(BuildContext context) {
-    // 'WillPopScope' is deprecated and shouldn't be used.
-    // Use PopScope instead.
-    // This feature was deprecated after v3.12.0-1.0.pre
-    return PopScope(
-        canPop: false,
-      onPopInvoked: (didPop) async  {
-          if (didPop) {
-            return;
-          }
-        final nav = Navigator.of(context);
-          bool value = await someFunc();
-          if (value) {
-            nav.pop();
-          }
-      },
-      child: Scaffold(
-        backgroundColor: Colors.blue,
-        appBar: AppBar(
-          leading: IconButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            icon: const Icon(Icons.arrow_back_ios_new),
-          ),
-          title: const Text('Will Pop Scope'),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('Page One'),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed('/two');
+              },
+              child: const Text('Next page'),
+            ),
+          ],
         ),
-        body: const Center(),
-      )
+      ),
+    );
+  }
+}
+
+class _PageTwo extends StatefulWidget {
+  const _PageTwo();
+
+  @override
+  State<_PageTwo> createState() => _PageTwoState();
+}
+
+class _PageTwoState extends State<_PageTwo> {
+  void _showBackDialog() {
+    showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Are you sure?'),
+          content: const Text(
+            'Are you sure you want to leave this page?',
+          ),
+          actions: <Widget>[
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              child: const Text('Nevermind'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              child: const Text('Leave'),
+              onPressed: () {
+                Navigator.pop(context);
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 
-  someFunc() {}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('Page Two'),
+            PopScope(
+              canPop: false,
+              onPopInvoked: (bool didPop) {
+                if (didPop) {
+                  return;
+                }
+                _showBackDialog();
+              },
+              child: TextButton(
+                onPressed: () {
+                  _showBackDialog();
+                },
+                child: const Text('Go back'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
