e61480b08d535bc148c6706422df45b1d320ebd7
jmcunst <xnslqjtmghaf@gmail.com>
2024-04-11 09:12:23 +0900

    feat : shoe homepage drawer

---
+++ lib/cloning/nike/pages/README.md
@@ -0,0 +1,5 @@
+# 1. Drawer Header
+
+## homepage.dart
+<img height="24%" width="24%" src="https://github.com/KRFLUTTERUG/wiki-flutter-widget/assets/17956765/c879f0d8-65ad-4572-ac7b-235f2c53b93f">
+
--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ import 'cloning/ecommerce/stackss_practical/screen/search.dart';
 import 'cloning/ecommerce/stackss_practical/screen/user_add.dart';
 import 'cloning/ecommerce/stackss_practical/screen/user_message.dart';
 import 'cloning/ecommerce/stackss_practical/screen/user_profile.dart';
+import 'cloning/nike/pages/homepage.dart';
 import 'cloning/spotify/data/model/album.dart';
 import 'cloning/spotify/data/model/album_track.dart';
 import 'cloning/spotify/di/service_locator.dart';
@@ -28,6 +29,7 @@ import 'cloning/spotify/ui/track_view_screen.dart';
 import 'package/20231011/counterprovider/main_counterprovider.dart';
 import 'package/20231011/userprovider/main_userprovider.dart';
 
+/* basic + spotify */
 // void main() {
 //   // Test 시 아래 2줄은 주석 처리 필요
 //   // WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
@@ -77,100 +79,106 @@ import 'package/20231011/userprovider/main_userprovider.dart';
 //     );
 //   }
 // }
-void main() {
-  runApp(const ProviderScope(child: MyApp()));
-}
+/* E-Commerce:stackss_practical */
+// void main() {
+//   runApp(const ProviderScope(child: MyApp()));
+// }
+//
+// class MyApp extends HookConsumerWidget {
+//   const MyApp({super.key});
+//
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context, WidgetRef ref) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData.light(),
+//       darkTheme: ThemeData.dark(),
+//       themeMode:
+//       ref.watch(appController).darkMode ? ThemeMode.dark : ThemeMode.light,
+//       home: MyHomePage(),
+//     );
+//   }
+// }
+//
+// class MyHomePage extends HookConsumerWidget {
+//   MyHomePage({super.key});
+//
+//   int _selectedIndex = 0;
+//
+//   final List<Widget> _children = [
+//     Home(),
+//     const UserSearch(),
+//     const UserAdd(),
+//     const UserMessage(),
+//     const UserAccount()
+//   ];
+//   @override
+//   Widget build(BuildContext context, WidgetRef ref) {
+//     void navigateBottomNavBar(int index) {
+//       // setState(() {
+//       //   _selectedIndex = index;
+//       // });
+//       ref.read(appController.notifier).navigateBottomNavBar(index);
+//     }
+//
+//     final state = ref.watch(appController);
+//     return Scaffold(
+//         appBar: AppBar(
+//           backgroundColor: state.primaryBackgroundColor,
+//           shadowColor: null,
+//           elevation: 0,
+//         ),
+//         body: _children[state.pageNo ?? 0],
+//         bottomNavigationBar: BottomNavigationBar(
+//           currentIndex: state.pageNo ?? 0,
+//           type: BottomNavigationBarType.fixed,
+//           onTap: navigateBottomNavBar,
+//           showUnselectedLabels: false,
+//           showSelectedLabels: false,
+//           selectedItemColor: Colors.purple[300],
+//           items: [
+//             const BottomNavigationBarItem(
+//                 icon: Icon(Icons.home), label: 'home'),
+//             const BottomNavigationBarItem(
+//                 icon: Icon(Icons.search), label: 'search'),
+//             BottomNavigationBarItem(
+//                 icon: Icon(
+//                   CupertinoIcons.add_circled_solid,
+//                   size: 40,
+//                   color: Colors.purple[300],
+//                 ),
+//                 label: 'Upload'),
+//             const BottomNavigationBarItem(
+//                 icon: Icon(CupertinoIcons.mail_solid), label: 'messages'),
+//             BottomNavigationBarItem(
+//                 icon: Container(
+//                   height: 40,
+//                   width: 40,
+//                   decoration: BoxDecoration(
+//                     borderRadius: BorderRadius.circular(80),
+//                     color: state.pageNo == 4 ? Colors.purple[300] : null,
+//                   ),
+//                   child: Padding(
+//                     padding: const EdgeInsets.all(4.0),
+//                     child: ClipRRect(
+//                       borderRadius: BorderRadius.circular(80),
+//                       child: Image.network(
+//                         'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZCldKgmO2Hs0UGk6nRClAjATKoF9x2liYYA&usqp=CAU',
+//                         fit: BoxFit.cover,
+//                         height: 30,
+//                         width: 30,
+//                       ),
+//                     ),
+//                   ),
+//                 ),
+//                 label: 'account'),
+//           ],
+//         ));
+//   }
+// }
+/* nike */
+import 'package:flutter/material.dart';
 
-class MyApp extends HookConsumerWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData.light(),
-      darkTheme: ThemeData.dark(),
-      themeMode:
-      ref.watch(appController).darkMode ? ThemeMode.dark : ThemeMode.light,
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends HookConsumerWidget {
-  MyHomePage({super.key});
-
-  int _selectedIndex = 0;
-
-  final List<Widget> _children = [
-    Home(),
-    const UserSearch(),
-    const UserAdd(),
-    const UserMessage(),
-    const UserAccount()
-  ];
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    void navigateBottomNavBar(int index) {
-      // setState(() {
-      //   _selectedIndex = index;
-      // });
-      ref.read(appController.notifier).navigateBottomNavBar(index);
-    }
-
-    final state = ref.watch(appController);
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: state.primaryBackgroundColor,
-          shadowColor: null,
-          elevation: 0,
-        ),
-        body: _children[state.pageNo ?? 0],
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: state.pageNo ?? 0,
-          type: BottomNavigationBarType.fixed,
-          onTap: navigateBottomNavBar,
-          showUnselectedLabels: false,
-          showSelectedLabels: false,
-          selectedItemColor: Colors.purple[300],
-          items: [
-            const BottomNavigationBarItem(
-                icon: Icon(Icons.home), label: 'home'),
-            const BottomNavigationBarItem(
-                icon: Icon(Icons.search), label: 'search'),
-            BottomNavigationBarItem(
-                icon: Icon(
-                  CupertinoIcons.add_circled_solid,
-                  size: 40,
-                  color: Colors.purple[300],
-                ),
-                label: 'Upload'),
-            const BottomNavigationBarItem(
-                icon: Icon(CupertinoIcons.mail_solid), label: 'messages'),
-            BottomNavigationBarItem(
-                icon: Container(
-                  height: 40,
-                  width: 40,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(80),
-                    color: state.pageNo == 4 ? Colors.purple[300] : null,
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(80),
-                      child: Image.network(
-                        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZCldKgmO2Hs0UGk6nRClAjATKoF9x2liYYA&usqp=CAU',
-                        fit: BoxFit.cover,
-                        height: 30,
-                        width: 30,
-                      ),
-                    ),
-                  ),
-                ),
-                label: 'account'),
-          ],
-        ));
-  }
-}
+void main() =>
+    runApp(MaterialApp(debugShowCheckedModeBanner: false, home: HomePage()));
